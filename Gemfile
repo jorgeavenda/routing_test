34acfc3abfa68da76150ffeb181c415e437c1296
jorge <jorgeavenda.11@gmail.com>
2019-09-10 21:20:03 -0300

    add seed data

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 
   gem 'rspec-rails', '~> 3.8'
+
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create cities
+10.times do
+  City.create(name: Faker::Nation.capital_city)
+end
+
+# Create loads
+['general', 'refrigerated'].each do |t|
+  Load.create(name: t, load_type: Load.load_types[t])
+end
+
+# Create Drivers without vehicle
+10.times do
+  Driver.create(name: Faker::Name.name, phone: Faker::PhoneNumber.cell_phone,
+                email: Faker::Internet.email)
+end
+
+# Create Vehicles without driver
+10.times do
+  Vehicle.create(capacity: Faker::Number.decimal(l_digits: 2), load_id: Faker::Number.between(from: 1, to: 2))
+end
+
+# Create Drivers with vehicle
+10.times do
+  driver = Driver.create(name: Faker::Name.name, phone: Faker::Number.number(digits: 9),
+                         email: Faker::Internet.email)
+  Vehicle.create(capacity: Faker::Number.decimal(l_digits: 2), load_id: Faker::Number.between(from: 1, to: 2),
+                 driver: driver)
+end
+
+# Set preferences_cities to drivers
+Driver.all.each do |driver|
+  driver.preferences_cities << City.find(
+    [*Faker::Number.between(from: 1, to: 10)..Faker::Number.between(from: 1, to: 10)])
+end
+
+# Create routes to be assigned driver and vehicle
+30.times do
+  range = Faker::Number.between(from: 1, to: 5) * 3600
+  starts_at = Faker::Time.between(from: Time.now.beginning_of_day, to: Time.now.end_of_day - range)
+  ends_at = starts_at + range
+  route = Route.create(starts_at: starts_at, ends_at: ends_at, load_id: Faker::Number.between(from: 1, to: 2),
+                       load_sum: Faker::Number.decimal(l_digits: 2),
+                       stops_amount: Faker::Number.between(from: 1, to: 10))
+  route.cities << City.find([*Faker::Number.between(from: 1, to: 10)..Faker::Number.between(from: 1, to: 10)])
+end
